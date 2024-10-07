0b6a29183de7f56721e69e93a7ee36d0d01c78ee
abdul-halim-dev <jamesborn1518@gmail.com>
2024-10-07 12:16:05 +0600

    imageloading

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+ 
+
+
+ 
+ 
+ const App = () => {
+   return (
+     <div className=" w-full  flex-wrap flex items-center justify-center gap-4 p-6 bg-slate-100 ">
+
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src=" https://i.ibb.co.com/xYjYqmj/masque1.jpg " alt="" />
+      </div>
+
+     
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/Bq5Vndt/masque2.jpg " alt="" />
+      </div>
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src=" https://i.ibb.co.com/xYjYqmj/masque1.jpg " alt="" />
+      </div>
+
+     
+ 
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/1MZCpJQ/masque3.jpg " alt="" />
+      </div>
+
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src=" https://i.ibb.co.com/xYjYqmj/masque1.jpg " alt="" />
+      </div>
+
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/1MZCpJQ/masque3.jpg " alt="" />
+      </div>
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/1MZCpJQ/masque3.jpg " alt="" />
+      </div>
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/1MZCpJQ/masque3.jpg " alt="" />
+      </div>
+      <div className=" w-[30%] h-[200px] rounded-md   ">
+        <img className=" w-full h-full object-cover " src="https://i.ibb.co.com/1MZCpJQ/masque3.jpg " alt="" />
+      </div>
+
+
+
+
+     </div>
+   )
+ }
+ 
+ export default App
